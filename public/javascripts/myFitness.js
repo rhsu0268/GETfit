8431971ad2aa6756b1d576fc9b06bb73b8296cd7
Richard Hsu <rhsu@gwu.edu>
2016-01-05 17:40:47 -0500

    Fixed routing problem

--- public/javascripts/myFitness.js
+++ public/javascripts/myFitness.js
@@ -1,3 +1,24 @@
 var app = angular.module('myFitness', ['ui.router']);
 
 console.log("Inside myfitness.js!");
+
+app.config([
+    '$stateProvider',
+    function($stateProvider)
+    {
+        $stateProvider.state('myFitness', {
+            url: '',
+            templateUrl: '/myFitness.html',
+            controller: 'MainCtrl'
+
+        });
+    }
+
+]);
+
+
+app.controller('MainCtrl', ['$scope', function($scope) {
+
+
+
+}]);
