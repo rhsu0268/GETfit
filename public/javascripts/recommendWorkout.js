a8e890984b4f7bf1695f255fc3c9c0cc00173c3e
Richard Hsu <rhsu@gwu.edu>
2016-01-17 14:22:40 -0500

    Added code to process user input in recommend workout

--- public/javascripts/recommendWorkout.js
+++ public/javascripts/recommendWorkout.js
@@ -14,13 +14,14 @@ app.config([
             controller: 'MainCtrl',
             resolve: {
 
-                postPromise: ['workouts', function(workouts) {
-                    //exercises.getAll();
+                workoutPromise: ['workouts', function(workouts) {
                     return workouts.getAll();
                 }],
                 postPromise: ['workouts', 'exercises', function(workouts, exercises) {
                     return exercises.getAll();
                 }]
+
+
 
             }
 
@@ -74,25 +75,28 @@ app.factory('exercises', ['$http', function($http) {
 app.controller('MainCtrl', ['$scope', 'workouts', 'exercises', '$stateParams', '$window', function($scope, workouts, exercises, $stateParams, $window) {
 
     $scope.workouts = workouts.workouts;
-    //console.log(exercises.exerciseService);
+    console.log($scope.workouts);
     $scope.exercises = exercises.exercises;
     //console.log($scope.exercises.exercises);
 
     var exercisesArray = [];
 
-    var count = 0;
     for (var exercise in $scope.exercises.exercises)
     {
         var tempObj = {};
         tempObj[exercise] = $scope.exercises[exercise];
         exercisesArray.push(tempObj);
-        count++;
-        console.log(count);
     }
     //console.log(count);
 
     console.log(exercisesArray.length);
     console.log(exercisesArray);
+
+    var userWorkouts = $scope.workouts;
+    console.log(userWorkouts);
+
+
+    // turn the workouts into an array
     $scope.recommendWorkout = function()
     {
         console.log("Inside recommend function!");
@@ -100,10 +104,77 @@ app.controller('MainCtrl', ['$scope', 'workouts', 'exercises', '$stateParams', '
         var userLevel = $scope.level;
         var intensity = $scope.intensity;
         var goal = $scope.goal;
+        console.log(goal);
 
-        var userWorkouts = $scope.workouts;
-        console.log(userWorkouts);
-        console.log($scope.exercises.length);
+
+        var sets;
+        var reps;
+        var exercisesNumber;
+
+        // optimization algorithm
+
+        var userLevelValue;
+        var intensityValue;
+
+        // userLevel - grab the exercises in the user's level
+        if (userLevel == "Beginner")
+        {
+            userLevelValue = Math.floor((Math.random() * 4) + 1);
+            console.log(userLevelValue);
+        }
+        else if (userLevel == "Intermediate")
+        {
+            userLevelValue = Math.floor((Math.random() * 3) + 5);
+            console.log(userLevelValue);
+        }
+        else if (userLevel == "Expert")
+        {
+            userLevelValue = Math.floor((Math.random() * 4) + 7);
+            console.log(userLevelValue);
+        }
+
+        // intensity - use a scale from 1 - 10 (score based on user's intensity) as well as age and BMI
+        if (intensity == "Low")
+        {
+            intensityValue = Math.floor((Math.random() * 4) + 1);
+            console.log(intensityValue);
+        }
+        else if (intensity == "Moderate")
+        {
+            intensityValue = Math.floor((Math.random() * 3) + 5);
+            console.log(intensityValue);
+        }
+        else if (intensity == "High")
+        {
+            intensityValue = Math.floor((Math.random() * 4) + 7);
+            console.log(intensityValue);
+        }
+
+        console.log(reps + " " + sets + " " + exercisesNumber);
+        // goal - look at the person's personal fitness goal (change reps and sets accordingly)
+        if (goal == "Maximal Strength")
+        {
+            // set range: Higher rep range: 1 - 5, less exercises
+            reps = 3;
+            sets = 5;
+            exercisesNumber = 2;
+            console.log(reps + " " + sets + " " + exercisesNumber);
+
+        }
+        else if (goal == "Muscle Building")
+        {
+            reps = 8;
+            sets = 3;
+            exercisesNumber = 3;
+        }
+        else if (goal == "Endurance")
+        {
+            // set range: Lower rep range: 12 - 15, more exercises
+            reps = 13;
+            sets = 1;
+            exercisesNumber = 4;
+            console.log(reps + " " + sets + " " + exercisesNumber);
+        }
 
     }
 }]);
