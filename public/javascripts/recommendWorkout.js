3ec21fcc4764a0b67b8ed59eef91b37668d62a98
Richard Hsu <rhsu@gwu.edu>
2016-01-17 21:02:32 -0500

    Started algorithm for userPreferences

--- public/javascripts/recommendWorkout.js
+++ public/javascripts/recommendWorkout.js
@@ -75,26 +75,24 @@ app.factory('exercises', ['$http', function($http) {
 app.controller('MainCtrl', ['$scope', 'workouts', 'exercises', '$stateParams', '$window', function($scope, workouts, exercises, $stateParams, $window) {
 
     $scope.workouts = workouts.workouts;
-    console.log($scope.workouts);
+    //console.log($scope.workouts);
     $scope.exercises = exercises.exercises;
-    console.log($scope.exercises.exercises.exercise1);
+    //console.log($scope.exercises.exercises.exercise1);
 
     var exercisesArray = [];
 
 
     for (var exercise in $scope.exercises.exercises)
     {
-        //var tempObj = {};
         var tempObj = $scope.exercises.exercises[exercise];
         exercisesArray.push(tempObj);
     }
-    //console.log(count);
 
     var exercisesCount = exercisesArray.length;
-    console.log(exercisesArray);
+    //console.log(exercisesArray);
 
     var userWorkouts = $scope.workouts;
-    console.log(userWorkouts);
+    //console.log(userWorkouts);
 
 
     // turn the workouts into an array
@@ -110,37 +108,45 @@ app.controller('MainCtrl', ['$scope', 'workouts', 'exercises', '$stateParams', '
 
         var sets;
         var reps;
-        var exercisesNumber;
+        var numExercises;
 
         // optimization algorithm
 
         var userLevelValue;
         var intensityValue;
 
+        var beginnerExercises = [];
+        var intermediateExercises = [];
+
         // userLevel - grab the exercises in the user's level
         if (userLevel == "Beginner")
         {
             userLevelValue = Math.floor((Math.random() * 4) + 1);
-            console.log(userLevelValue);
+            //console.log(userLevelValue);
 
             // get the workouts that belong to beginner category
-
-            var beginnerExercises = [];
-
             for (var i = 0; i < exercisesCount; i++)
             {
-                console.log(exercisesArray[i]);
+                //console.log(exercisesArray[i]);
                 if (exercisesArray[i].level == "beginner")
                 {
                     beginnerExercises.push(exercise);
                 }
             }
-            console.log(beginnerExercises.length);
         }
         else if (userLevel == "Intermediate")
         {
             userLevelValue = Math.floor((Math.random() * 3) + 5);
-            console.log(userLevelValue);
+            //console.log(userLevelValue);
+
+            for (var i = 0; i < exercisesCount; i++)
+            {
+                //console.log(exercisesArray[i]);
+                if (exercisesArray[i].level == "beginner")
+                {
+                    intermediateExercises.push(exercise);
+                }
+            }
         }
         else if (userLevel == "Expert")
         {
@@ -149,7 +155,10 @@ app.controller('MainCtrl', ['$scope', 'workouts', 'exercises', '$stateParams', '
         }
 
         // intensity - use a scale from 1 - 10 (score based on user's intensity) as well as age and BMI
-        if (intensity == "Low")
+        // use it to determine the rest time in between groups of workouts
+
+        var restTime;
+        if (intensity == "High")
         {
             intensityValue = Math.floor((Math.random() * 4) + 1);
             console.log(intensityValue);
@@ -159,36 +168,36 @@ app.controller('MainCtrl', ['$scope', 'workouts', 'exercises', '$stateParams', '
             intensityValue = Math.floor((Math.random() * 3) + 5);
             console.log(intensityValue);
         }
-        else if (intensity == "High")
+        else if (intensity == "Low")
         {
             intensityValue = Math.floor((Math.random() * 4) + 7);
             console.log(intensityValue);
         }
 
-        console.log(reps + " " + sets + " " + exercisesNumber);
+        console.log(reps + " " + sets + " " + numExercises);
         // goal - look at the person's personal fitness goal (change reps and sets accordingly)
         if (goal == "Maximal Strength")
         {
             // set range: Higher rep range: 1 - 5, less exercises
             reps = 3;
             sets = 5;
-            exercisesNumber = 2;
-            console.log(reps + " " + sets + " " + exercisesNumber);
+            numExercises = 2;
+            console.log(reps + " " + sets + " " + numExercises);
 
         }
         else if (goal == "Muscle Building")
         {
             reps = 8;
             sets = 3;
-            exercisesNumber = 3;
+            numExercises = 3;
         }
         else if (goal == "Endurance")
         {
             // set range: Lower rep range: 12 - 15, more exercises
             reps = 13;
             sets = 1;
-            exercisesNumber = 4;
-            console.log(reps + " " + sets + " " + exercisesNumber);
+            numExercises = 4;
+            console.log(reps + " " + sets + " " + numExercises);
         }
 
 
@@ -197,7 +206,17 @@ app.controller('MainCtrl', ['$scope', 'workouts', 'exercises', '$stateParams', '
         // # 1 - Based on user preferences
 
         // narrow down results based on user's preferences
-
+        console.log(numExercises);
+        var workout1 = [];
+        for (var i = 0; i < numExercises; i++)
+        {
+            if (userLevel == "Beginner")
+            {
+                var index = Math.floor((Math.random() * beginnerExercises.length));
+                console.log(index);
+                workout1.push(beginnerExercises[index]);
+            }
+        }
 
 
         // # 2 - Based on user's planned workouts
