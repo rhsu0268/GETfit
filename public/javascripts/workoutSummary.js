bccb5a7029d8cb32e989eb2ca71ab148ad36dd70
Richard Hsu <rhsu@gwu.edu>
2016-01-20 23:01:24 -0500

    Added workoutSummary front end

---
+++ public/javascripts/workoutSummary.js
@@ -0,0 +1,84 @@
+var app = angular.module('workoutSummary', ['ui.router']);
+
+console.log("Inside workoutSummary.js!");
+
+app.config([
+    '$stateProvider',
+    function($stateProvider)
+    {
+        $stateProvider.state('workoutSummary', {
+            url: '',
+            templateUrl: '/workoutSummary.html',
+            controller: 'MainCtrl',
+            resolve: {
+
+                postPromise: ['workouts', function(workouts) {
+                    return workouts.getAll();
+                }]
+            }
+
+        });
+
+    }
+
+]);
+
+app.factory('workouts', ['$http', function($http) {
+
+    var workoutService = {
+        workouts: []
+    };
+
+    workoutService.getAll = function() {
+        return $http.get('/workouts').success(function(data)
+        {
+            angular.copy(data, workoutService.workouts);
+            console.log(data);
+
+        });
+
+    };
+
+    workoutService.create = function(workout)
+    {
+        return $http.post('/workouts', workout).success(function (data)
+        {
+            //console.log(data);
+            workoutService.workouts.push(data);
+        });
+    };
+
+
+    workoutService.get = function(id)
+    {
+        //console.log('inside workoutService.get');
+        return $http.get('/workouts/' + id).then(function (res) {
+            //console.log(res);
+            return res.data;
+        });
+    };
+
+    workoutService.remove = function(id)
+    {
+        return $http.get('/deleteWorkout/' + id).then(function (res) {
+            return res.data;
+        });
+    }
+
+    workoutService.update = function(workout)
+    {
+        console.log("inside update");
+        return $http.post('/updateWorkout/' + workout._id, workout).success(function (data) {
+            console.log("inside update");
+            console.log(data);
+
+        });
+    }
+    return workoutService;
+
+}]);
+
+app.controller('MainCtrl', ['$scope', 'workouts', '$stateParams', '$window', function($scope, workouts, $stateParams, $window) {
+
+
+}]);
