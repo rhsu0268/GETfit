d9f19586acd4752b1c058e03aff3fa8551b3d349
Richard Hsu <rhsu@gwu.edu>
2016-01-30 17:59:57 -0500

    Added back end for login and register

--- routes/index.js
+++ routes/index.js
@@ -245,6 +245,7 @@ router.post('/findUpdateCompletedWorkout', function(req, res, next) {
 });
 */
 
+
 router.get('/deleteCompletedWorkouts', function(req, res) {
     //Workout.findById
     WorkoutSummary.remove({}, function(err) {
@@ -256,6 +257,71 @@ router.get('/deleteCompletedWorkouts', function(req, res) {
     res.send("successfully deleted completed workouts!");
 
 });
+
+
+
+var passport = require('passport');
+var User = mongoose.model('User');
+
+// logging and logging out
+router.post('/register', function(req, res, next) {
+
+    if (!req.body.username || !req.body.password )
+    {
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+    var user = new User();
+
+    user.username = req.body.username;
+
+    user.setPassword(req.body.password);
+
+    user.save(function(err) {
+        if (err)
+        {
+            return next(err);
+        }
+        return res.json({token: user.generateJWT()});
+    });
+});
+
+router.post('/login', function(req, res, next) {
+    if (!req.body.username || !req.body.password)
+    {
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+
+    passport.authenticate('local', function(err, user, info) {
+
+        if (err)
+        {
+            return next(err);
+        }
+
+        if (user)
+        {
+            return res.json({token: user.generateJWT()});
+        }
+        else
+        {
+            return res.status(401).json(info);
+        }
+
+
+    })(req, res, next);
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
