6ba9e56328bc335e43538af0ab05b9b1c6e51037
Richard Hsu <rhsu@gwu.edu>
2016-01-30 11:49:23 -0500

    Added Users model

---
+++ models/Users.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+
+var UserShcema = new mongoose.Schema({
+
+    username: {type: String, lowercase:true, unique: true},
+    hash: String,
+    salt: String
+
+
+});
+
+
+Userschema.methods.setPassword = function(password)
+{
+    this.salt = crypto.randomBytes(16).toString('hex');
+
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+
+Userschema.methods.validPassword = function(password)
+{
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+    return this.hash === hash;
+};
+
+mongoose.model('User', UserSchema);
