11357774b5198903ba189dfb195609ab5b157fad
Richard Hsu <rhsu@gwu.edu>
2016-01-30 12:00:31 -0500

    Added JWT jsonwebtoken

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
-var UserShcema = new mongoose.Schema({
+var UserSchema = new mongoose.Schema({
 
     username: {type: String, lowercase:true, unique: true},
     hash: String,
@@ -11,7 +12,7 @@ var UserShcema = new mongoose.Schema({
 });
 
 
-Userschema.methods.setPassword = function(password)
+UserSchema.methods.setPassword = function(password)
 {
     this.salt = crypto.randomBytes(16).toString('hex');
 
@@ -19,10 +20,25 @@ Userschema.methods.setPassword = function(password)
 };
 
 
-Userschema.methods.validPassword = function(password)
+UserSchema.methods.validPassword = function(password)
 {
     var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
     return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function()
+{
+    // set expiration to 60 displays
+    var today = new Date();
+    var exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        _id: this._id,
+        username: this.username,
+        exp: parseInt(exp.getTime() / 1000),
+    }, 'SECRET');
+
 };
 
 mongoose.model('User', UserSchema);
